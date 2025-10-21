b2610bf1f05b9925105b627d57969a7777b8209d
Tinotenda Muzovaka <tinotendamuzovaka5@gmail.com>
2025-10-21 10:03:55 +0100

    Added new database tables

--- models.py
+++ models.py
@@ -11,9 +11,10 @@ class Base(DeclarativeBase):
 class User(UserMixin, Base):
     __tablename__ = 'users'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
-    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
     password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    role: Mapped[str] = mapped_column(String(50), nullable=False, default='user')
 
     def __repr__(self):
         return f"<User {self.email}>"
@@ -21,3 +22,21 @@ class User(UserMixin, Base):
     def gravatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+
+class Course(Base):
+    __tablename__ = 'courses'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(150), nullable=False)
+    category: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    def __repr__(self):
+        return f"<Course {self.title}>"
+
+class Enrollment(Base):
+    __tablename__ = 'enrollments'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    course_id: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<Enrollment User ID: {self.user_id}, Course ID: {self.course_id}>"
